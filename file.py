f8041fd3f2d8c7b3c1ebc9b3707a7b42c5b233ba
mayankshah97 <mayankshah97@gmail.com>
2019-02-12 12:49:44 +0530

    M

--- file.py
+++ file.py
@@ -1,21 +1,16 @@
 class Calc(object):
-<<<<<<< HEAD
 	"""docstring for Calc"""
 	def __init__(self, arg):
-<<<<<<< HEAD
 		super(Calc, self).__init__()
 		self.arg = arg
 	def addition(self, a, b):
 		self.a = a
 		self.b = b
 		c = a + b
-=======
 		super Calc, self).__init__()
 		self.arg = arg
 	
 	def multiplication(self ,a,b):
 			c = a*b
->>>>>>> 1c93ed2851e71b9d5d7dedf6d350c656cacb875e
-=======
     def division
->>>>>>> 3a2cebed7837aa3009daece95b63836fe0cf8826
+
